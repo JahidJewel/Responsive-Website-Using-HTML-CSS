23e86be8c7d6c4cd975f1ca01e808682e660b3ee
Jahidul Islam <50165206+JahidJewel@users.noreply.github.com>
2020-06-24 23:24:17 -0400

    Add files via upload

---
+++ Blogger, Responsive website/js/main.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+
+
+    $nav = $('.nav');
+    $toggleCollapse = $('.toggle-collapse');
+
+    /** click event on toggle menu */
+
+    $toggleCollapse.click(function(){
+
+        $nav.toggleClass('collapse');
+
+
+
+    })
+
+    //owl-carousel for blog
+    $('.owl-carousel').owlCarousel(
+       /* { loop:true,
+            autoplay: true,
+            autoplay Timeout: 3000,
+            dots: false,
+            nav: true,
+            navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next') ]
+        } */
+    );
+
+
+
+    
+    //click to scroll top
+    $('.move-up span').click(function(){
+        $('html,body').animate({
+            scrollTop:0
+        },2000);
+    })
+
+})
